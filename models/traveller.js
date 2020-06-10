89ab3c72dca6f84de77f54bd327bcb33fa7f0760
Donald Cameron <drc85@live.co.uk>
2020-06-10 16:49:52 +0100

    MPV and first extension completed

--- models/traveller.js
+++ models/traveller.js
@@ -3,23 +3,38 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  const startLocations = this.journeys.map((journey) => {
+    return journey.startLocation
+  });
+  return startLocations;
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  const endLocations = this.journeys.map((journey) => {
+    return journey.endLocation
+  });
+  return endLocations;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  const result = this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  });
+return result;
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  const result = this.journeys.filter((journey) => {
+    return journey.distance > minDistance;
+  })
+  return result
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  const result = this.journeys.reduce((total, journey) => {
+    return total + journey.distance;
+  }, 0);
+  return result;
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
